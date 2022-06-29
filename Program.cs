073f8a9b40e3186f7baa7c8454ed934bd5dd5995
Владислав Берестюк <vladixerplay@gmail.com>
2022-06-29 16:33:03 +0300

    ПРИВ

--- Program.cs
+++ Program.cs
@@ -1,6 +1,2 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Hello, World!");
-Console.WriteLine("Privet!");
-Console.WriteLine("Prive1!");
-Console.WriteLine("Prive2!");
-//top
